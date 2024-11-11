65ec3ab8b92a17874a42b7953e20a927d2e6ee9a
Ashraful <smashrafulcse@gmail.com>
2024-11-11 15:32:45 +0600

    connected backend to shared host api

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,13 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: process.env.NODE_ENV === 'production' ? 'https' : 'http',  // Use HTTPS in production
+                protocol: process.env.NODE_ENV === 'production' ? 'http' : 'http',  // Use HTTPS in production
                 hostname: process.env.NEXT_PUBLIC_BACKEND_URL ? new URL(process.env.NEXT_PUBLIC_BACKEND_URL).hostname : 'localhost',
                 port: process.env.NEXT_PUBLIC_BACKEND_URL && new URL(process.env.NEXT_PUBLIC_BACKEND_URL).port ? new URL(process.env.NEXT_PUBLIC_BACKEND_URL).port : (process.env.NODE_ENV === 'production' ? '' : '8000'),
                 pathname: '/storage/**',
             },
         ],
-        domains: ['51.20.18.159', 'your-other-allowed-domains.com'],
+        domains: ['51.20.18.159', 'ecom-lara-api2.expertoftech.com'],
     },
 };
 
