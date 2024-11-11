8de0b23621b5f9ab709e67ba9b1471d158386fef
Ashraful <smashrafulcse@gmail.com>
2024-11-11 15:50:09 +0600

    api SSl installed

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: process.env.NODE_ENV === 'production' ? 'http' : 'http',  // Use HTTPS in production
+                protocol: process.env.NODE_ENV === 'production' ? 'https' : 'http',  // Use HTTPS in production
                 hostname: process.env.NEXT_PUBLIC_BACKEND_URL ? new URL(process.env.NEXT_PUBLIC_BACKEND_URL).hostname : 'localhost',
                 port: process.env.NEXT_PUBLIC_BACKEND_URL && new URL(process.env.NEXT_PUBLIC_BACKEND_URL).port ? new URL(process.env.NEXT_PUBLIC_BACKEND_URL).port : (process.env.NODE_ENV === 'production' ? '' : '8000'),
                 pathname: '/storage/**',
